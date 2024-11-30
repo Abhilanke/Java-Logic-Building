03b1a8f3d8578d175820430b0bf8f8dcbad6de65
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2024-11-30 20:19:29 +0530

    Add files via upload

---
+++ A1.java
@@ -0,0 +1,16 @@
+package logicbuilding;
+//1What will be  output Below code
+//a)028
+//b)28
+//c)0
+//d)Error
+
+public class A1 {
+
+	public static void main(String[] args) {
+		int i= 028;
+		System.out.println(i);
+
+	}
+
+}
---
+++ A2.java
@@ -0,0 +1,16 @@
+package logicbuilding;
+//what is outpute below code
+//A)1248.0
+//B)7_6_7
+//C)can not be integer value
+//D)1248
+public class A2 {
+
+	public static void main(String[] args) {
+		float x=7_6_7;
+		float y=4_8_1;
+		System.out.println(x+y);
+
+	}
+
+}
