14c7f23409f94025771f08a19d67cea03b93d7ec
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2024-12-12 11:52:00 +0530

    Add files via upload

---
+++ Fectorial.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Fectorial { //5!=5*4*3*2*1=120;
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter your number :");
+        int num=scan.nextInt();
+        int a=1;
+        for(int i=1;i<=num;i++){
+             a = a * i;
+        }
+        System.out.println(a);
+
+    }
+}
---
+++ Tcs.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tcs {
+    public static void main(String[] args) {
+        // List of strings
+        List<String> stringList = List.of("Abhi", "Abhishek", "piu", "Priyanka", "Rabbit");
+
+        // Loop through the list and check for palindromes
+        for (String str : stringList) {
+            if (isPalindrome(str)) {
+                System.out.println(str + " is a palindrome");
+            }
+        }
+    }
+
+    // Method to check if a string is a palindrome
+    public static boolean isPalindrome(String str) {
+        String lowerStr = str.toLowerCase(); // Convert to lowercase for case-insensitive check
+        int left = 0, right = lowerStr.length() - 1;
+
+        while (left < right) {
+            if (lowerStr.charAt(left) != lowerStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
