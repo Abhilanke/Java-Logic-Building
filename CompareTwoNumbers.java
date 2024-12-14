486b703435002ad0fdb3a39f25a8353a20f06b11
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2024-12-14 12:17:28 +0530

    Add files via upload

---
+++ CompareTwoNumbers.java
@@ -0,0 +1,60 @@
+package logicbuilding;
+//32. Compare Two Numbers
+//
+//Write a Java program to compare two numbers.
+//Input Data:
+//Input first integer: 25
+//Input second integer: 39
+//Expected Output
+//
+//25 != 39                                                                          
+//25 < 39                                                                           
+//25 <= 39
+import java.util.Scanner;
+
+public class CompareTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter your First Number :>> ");
+		int num1=scan.nextInt();
+		System.out.print("Enter your Second Number :>> ");
+		int num2=scan.nextInt();
+		
+		if(num1 != num2) {
+			System.out.println(num1+" != "+num2);
+		}
+		else {
+			System.out.println(num1+" = "+num2);
+			
+		}
+		if(num1 < num2) {
+			System.out.println(num1+" < "+num2);
+			
+		}
+		if(num1 <= num2) {
+			System.out.println(num1+" <= "+num2);
+			
+		}else {
+			System.out.println(num1+" >= "+num2);
+			
+		}
+		
+//		// Compare and display the results
+//        if (num1 == num2)
+//            System.out.printf("%d == %d\n", num1, num2);
+//        if (num1 != num2)
+//            System.out.printf("%d != %d\n", num1, num2);
+//        if (num1 < num2)
+//            System.out.printf("%d < %d\n", num1, num2);
+//        if (num1 > num2)
+//            System.out.printf("%d > %d\n", num1, num2);
+//        if (num1 <= num2)
+//            System.out.printf("%d <= %d\n", num1, num2);
+//        if (num1 >= num2)
+//            System.out.printf("%d >= %d\n", num1, num2);
+		
+
+	}
+
+}
