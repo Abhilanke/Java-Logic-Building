6be49ec6fcc20adff0d504b3f43089b275cec89a
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-01-05 15:09:49 +0530

    Add files via upload

---
+++ HexagonArea.java
@@ -0,0 +1,21 @@
+package logicbuilding;
+
+import java.util.Scanner;
+
+public class HexagonArea {
+
+	public static void main(String[] args) {
+		System.out.println("Input the length of a side of the hexagon >> ");
+		Scanner scan =new Scanner(System.in);
+		int s=scan.nextInt();
+		
+		
+		 // Calculate and display the area of the hexagon
+        System.out.print("The area of the hexagon is: " + hexagonArea(s) + "\n");
+    }
+
+    public static double hexagonArea(double s) {
+        // Calculate the area of a hexagon based on its side length
+        return (6 * (s * s)) / (4 * Math.tan(Math.PI / 6));
+    }
+}
