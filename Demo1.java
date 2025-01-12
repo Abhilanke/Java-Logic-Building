a0b971bf00085abd02c0b1491f9cb14237c4d4f1
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-01-12 19:14:50 +0530

    Update Demo1.java

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo1 {
+public class Demo1{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter First Number");
