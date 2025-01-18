9cba74a3960e9d962251c02a2e452d86120bb7e3
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-01-18 19:47:33 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,15 @@
+package logicBuilding1;
+
+public class a {
+	public static void main(String[] args) {
+		System.out.println("hello wolrd");//output lene ke liye use kiya hai ("content");Output.
+		
+		
+//		public-Access EveryBuddy
+//		Static-create one copy
+//		void-no value return
+//		main-main method
+//		string args-
+	}
+
+}
