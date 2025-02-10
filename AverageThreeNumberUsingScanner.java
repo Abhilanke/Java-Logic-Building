b45851b136da219fbd52471303c4dc2d258291f8
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-02-11 02:31:54 +0530

    Update AverageThreeNumberUsingScanner.java

--- AverageThreeNumberUsingScanner.java
+++ AverageThreeNumberUsingScanner.java
@@ -1,6 +1,5 @@
 package logicbuilding;
 //12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
-
 import java.util.Scanner;
 
 public class AverageThreeNumberUsingScanner {
