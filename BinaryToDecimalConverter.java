c42a81a62d34d2b9366d593aeaa2c650f662abf5
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-03-02 06:55:55 +0530

    Update BinaryToDecimalConverter.java

--- BinaryToDecimalConverter.java
+++ BinaryToDecimalConverter.java
@@ -1,13 +1,11 @@
 package logicbuilding;
 //22. Binary to Decimal Converter
-//
 //Write a Java program to convert a binary number to a decimal number.
 //Input Data:
 //Input a binary number: 100
 //Expected Output
 //Decimal Number: 4 
 import java.util.Scanner;
-
 public class BinaryToDecimalConverter {
 
 	public static void main(String[] args) {
