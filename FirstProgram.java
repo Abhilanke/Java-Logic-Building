9d14f878fa4e5916d87809de2e08995bb568a75f
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-03-23 11:13:04 +0530

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,9 +1,7 @@
 package logicbuilding;
 //1.Write a Java program to print 'Hello' on screen and your name on a separate line.
-
 //Expected Output :-
 //Hello
-
 //Alexandra Abramov
 public class FirstProgram {
 
