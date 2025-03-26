170336ee19a34ef12939c08df0a1950d07a938e1
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-03-26 08:19:23 +0530

    Update PrintJavaName.java

--- PrintJavaName.java
+++ PrintJavaName.java
@@ -5,6 +5,7 @@ package logicbuilding;
 //   J   a a   v   v  a a                                                 
 //J  J  aaaaa   V V  aaaaa                                                
 // JJ  a     a   V  a     a
+
 public class PrintJavaName {
 	
 	public static void main(String[] args) {
