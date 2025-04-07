cb8a0a7137a59e1de28880a9be6bfdd450997f56
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-04-07 19:02:29 +0530

    Update EvenOddModulo.java

--- Interview/EvenOddModulo.java
+++ Interview/EvenOddModulo.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class EvenOddModulo {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Enter Your number :");
 		Scanner scan=new Scanner(System.in);
 		int even=scan.nextInt();
+		
 		if(even % 2==0) {
 			System.out.println("Even number ");
 			
