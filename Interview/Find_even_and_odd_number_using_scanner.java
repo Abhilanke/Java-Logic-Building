e75a96c2f28b471973ee0aea5f7be2d4bca191d0
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-04-09 09:55:28 +0530

    Update Find_even_and_odd_number_using_scanner.java

--- Interview/Find_even_and_odd_number_using_scanner.java
+++ Interview/Find_even_and_odd_number_using_scanner.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Find_even_and_odd_number_using_scanner {
 
 	public static void main(String[] args) {
+		
 		Scanner scan =new Scanner(System.in);
 		System.out.print("Enter Any Number : ");
 		int number=scan.nextInt();
