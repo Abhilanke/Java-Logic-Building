1b36b95de5357c7980967c89717b292482b7c3fe
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-04-17 09:45:31 +0530

    Update ConvertDecimalToBinary.java

--- Basic To Advance/ConvertDecimalToBinary.java
+++ Basic To Advance/ConvertDecimalToBinary.java
@@ -3,6 +3,7 @@ package logicbuilding;
 public class ConvertDecimalToBinary {
 
 	public static void main(String[] args) {
+		
 		int num=11;
 		String binary =Integer.toBinaryString(num);
 		System.out.println(binary);
