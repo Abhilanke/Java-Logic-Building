6ece7afc83593b38617deedfd9f8b9dcafacbcdd
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-04-24 12:43:48 +0530

    Update AgeCondition.java

--- Interview/AgeCondition.java
+++ Interview/AgeCondition.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AgeCondition {
 
 	public static void main(String[] args) {
+		
 		System.out.print("Enter Your Age :");
 		Scanner scan=new Scanner(System.in);
 		int age=scan.nextInt();
