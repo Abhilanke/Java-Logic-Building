8aadfff0a3cdb0d6fb08bf704b2d4ba14aceba34
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-05-07 08:31:19 +0530

    Update Calculate_the_sum_of_digit_of_a_number.java

--- Interview/Calculate_the_sum_of_digit_of_a_number.java
+++ Interview/Calculate_the_sum_of_digit_of_a_number.java
@@ -24,6 +24,7 @@ public class Calculate_the_sum_of_digit_of_a_number {
 			sum = sum+digit;//add the digit to sum
 			number =number/10;//remove the last digit from number
 		}
+		
 		return sum;
 	}
 
