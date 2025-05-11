4562f0c50a02519996c0835c44543d2bfb4d616b
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-05-11 15:36:34 +0530

    Update Find_even_and_odd_number_using_scanner.java

--- Interview/Find_even_and_odd_number_using_scanner.java
+++ Interview/Find_even_and_odd_number_using_scanner.java
@@ -9,6 +9,7 @@ public class Find_even_and_odd_number_using_scanner {
 		Scanner scan =new Scanner(System.in);
 		System.out.print("Enter Any Number : ");
 		int number=scan.nextInt();
+		
 		if(number%2==0) {
 			System.out.println("Number is even : "+number);
 		}else {
