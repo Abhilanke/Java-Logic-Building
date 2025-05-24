eb3636604cfab0b9758e6a0ff32f41e6f3536ae4
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-05-24 08:50:57 +0530

    Update Find_armstrong_number.java

--- Interview/Find_armstrong_number.java
+++ Interview/Find_armstrong_number.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class Find_armstrong_number {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	
 		int arm=0, a,b,c,d,no;
+	
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter any number : ");
+
 		no =scan.nextInt();
+
 		d=no;
+
 		while(no>0) {
 			a=no%10;
 			no=no/10;
