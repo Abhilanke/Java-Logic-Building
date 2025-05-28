cb1b2dc1c5ff5c369e29916fd2c32ba5402a9558
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-05-28 10:26:24 +0530

    Update Find_factorial_on_given_number_using_scanner.java

--- Interview/Find_factorial_on_given_number_using_scanner.java
+++ Interview/Find_factorial_on_given_number_using_scanner.java
@@ -8,7 +8,9 @@ public class Find_factorial_on_given_number_using_scanner {
 		
 		int factorial=1;
 		Scanner scan=new Scanner(System.in);
+		
 		System.out.print("Enter number : ");
+		
 		int number=scan.nextInt();
 		
 		for(int i=1;i<=number;i++) {
