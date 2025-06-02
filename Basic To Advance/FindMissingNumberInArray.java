a4a39f6c98914ffcc1ac3ae68c60b987eace35d3
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-06-02 13:46:13 +0530

    Update FindMissingNumberInArray.java

--- Basic To Advance/FindMissingNumberInArray.java
+++ Basic To Advance/FindMissingNumberInArray.java
@@ -5,8 +5,11 @@ public class FindMissingNumberInArray {
 	public static void main(String[] args) {
 
         int[] arr= {1,2,4,5};
+		
         int n=arr.length+1;
+		
         int total=n*(n+1)/2;
+		
         for(int num :arr) {
         	total-=num;
         }
