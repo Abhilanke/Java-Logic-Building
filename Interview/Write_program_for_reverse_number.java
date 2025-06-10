aa7ed8b89dbb9a59e222e750b0a1cca068924bc8
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-06-10 07:43:00 +0530

    Update Write_program_for_reverse_number.java

--- Interview/Write_program_for_reverse_number.java
+++ Interview/Write_program_for_reverse_number.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Write_program_for_reverse_number {
 
 	public static void main(String[] args) {
+		
 		int no ,rev=0,r,a;
 		Scanner scan=new Scanner(System.in);
 		System.out.println("Enter any number (123456) : ");
 		no=scan.nextInt();
+		
 		a=no;
+		
 		while(no>0) {
 			r=no%10;
 			rev=rev*10+r;
