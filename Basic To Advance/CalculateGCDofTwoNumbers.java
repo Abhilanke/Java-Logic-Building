58303f4659c1a7da3a30bc4fc0178763d4fb6a92
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-06-11 08:03:49 +0530

    Update CalculateGCDofTwoNumbers.java

--- Basic To Advance/CalculateGCDofTwoNumbers.java
+++ Basic To Advance/CalculateGCDofTwoNumbers.java
@@ -3,8 +3,11 @@ package logicbuilding;
 public class CalculateGCDofTwoNumbers {
 
 	public static void main(String[] args) {
+		
 		int a=62,b=48;
+		
 		while(b!=0) {
+			
 			int temp=b;
 			b=a%b;
 			a=temp;
