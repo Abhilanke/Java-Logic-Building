40a0fe91fb8f18f651ff6d9e3550d7b24351dea9
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-06-25 19:28:12 +0530

    Update CountTheNumberOFDigitsinNumber.java

--- Basic To Advance/CountTheNumberOFDigitsinNumber.java
+++ Basic To Advance/CountTheNumberOFDigitsinNumber.java
@@ -2,7 +2,9 @@ package logicbuilding;
 
 public class CountTheNumberOFDigitsinNumber  {
 	public static void main(String[] args) {
+		
 		int num=12323234;
+		
 		int count=String.valueOf(num).length();
 		System.out.println(count);
 
