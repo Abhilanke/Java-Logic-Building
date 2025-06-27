4463dbb7a35587f0d72ae8e21c838184ed03e71d
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-06-27 22:09:48 +0530

    Update CheckIftheNumberisArmstrong.java

--- Basic To Advance/CheckIftheNumberisArmstrong.java
+++ Basic To Advance/CheckIftheNumberisArmstrong.java
@@ -3,6 +3,7 @@ package logicbuilding;
 public class CheckIftheNumberisArmstrong {
 
 	public static void main(String[] args) {
+		
 		int num=153,sum=0,temp=num;
 		while(temp !=0) {
 			int digit=temp%10;
