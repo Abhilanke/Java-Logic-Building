1f04c483eb2f8b4412521c4ef85990594f1ca5da
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-07-20 09:07:31 +0530

    Update A3.java

--- A3.java
+++ A3.java
@@ -4,6 +4,8 @@ package logicbuilding;
 //4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
 //Expected Output
 //2.9760461760461765
+
+
 public class A3 {
 
 	public static void main(String[] args) {
