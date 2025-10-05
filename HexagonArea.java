9ad221ada693cb4f9bd86d83b0b7c9a8cfb557ae
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-10-05 15:13:15 +0530

    Update HexagonArea.java

--- HexagonArea.java
+++ HexagonArea.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class HexagonArea {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Input the length of a side of the hexagon >> ");
 		Scanner scan =new Scanner(System.in);
 		int s=scan.nextInt();
-		
 		
 		 // Calculate and display the area of the hexagon
         System.out.print("The area of the hexagon is: " + hexagonArea(s) + "\n");
@@ -19,3 +19,4 @@ public class HexagonArea {
         return (6 * (s * s)) / (4 * Math.tan(Math.PI / 6));
     }
 }
+
