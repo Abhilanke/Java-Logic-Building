32b2454eff3ec559f3131b1dbe3fe76151596039
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-11-05 12:03:43 +0530

    Reformat A3.java with consistent spacing

--- A3.java
+++ A3.java
@@ -7,10 +7,13 @@ package logicbuilding;
 public class A3 {
 
 	public static void main(String[] args) {
+
+		
 		double formula=4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
 		System.out.println(formula);
 
 	}
 
 }
+
 
